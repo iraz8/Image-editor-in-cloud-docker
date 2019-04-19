029f3e43a98c7a1e84de9ee510e2687677520c0c
Razvan <razvanboghiu@outlook.com>
2019-04-19 14:19:08 +0300

    V1,5 Some fixes and moving from Windows to Linux

--- GaussianFilterServer/src/GaussianFilterServer.java
+++ GaussianFilterServer/src/GaussianFilterServer.java
@@ -1,7 +1,16 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
 
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Size;
+
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
 
 class GaussianFilterServer {
 
@@ -47,7 +56,7 @@ class Handler extends Thread {
     public static final int NUM_THREADS = 10;
     final static String FIXEDHOSTNAME = "localhost";
     final static int GaussianFilterServerPort = 20001;
-
+    static final Path currentPath = FileSystems.getDefault().getPath(".");
 
     private ServerSocket serverSocket;
     private int threadNumber;
@@ -116,12 +125,12 @@ class Handler extends Thread {
 
     private String getParameterFromMsg(String msg) {
         String[] msgSplited = msg.split(" ");
-        return msgSplited[1];
+        return msgSplited[2];
     }
 
     private String getPathFromMsg(String msg) {
         String[] msgSplited = msg.split(" ");
-        return msgSplited[2];
+        return msgSplited[1];
     }
 
     private String getMsgFromClient(BufferedReader is) throws IOException {
@@ -147,25 +156,62 @@ class Handler extends Thread {
         //DEBUG
         System.out.println(msgToSend);
 
-/*
-        Socket specializedServer;
+        GaussianFilter filter = new GaussianFilter();
+        filter.apply(path,parameter);
 
-        switch(command) {
-            case "GAUSSIAN-FILTER":
-                specializedServer = "localhost" + GaussianFilterServerPort;
-
-        switch (command) {
-            case "GAUSSIAN-FILTER":
-
-                break;
-            case "CLOSE":
-                closeConnection(inSocket);
-                break;
-            default:
-                System.err.println("ERROR! Method: int decisionMaker(String line)! Unknown command! Command:" + command);
-                break;
-        }*/
         closeConnection(inSocket);
 
+    }
+}
+
+class GaussianFilter {
+    static final String currentPath = new File("").getAbsolutePath();
+    String getFilename (String path) {
+        File f = new File(path);
+        return f.getName();
+    }
+    void apply(String path,String parameters){
+
+        try {
+
+            System.out.println(currentPath + "/Images/[Processed]" +  getFilename(path));
+            File f = null;
+            String OS = System.getProperty("os.name").toLowerCase();
+
+           if (OS.contains("win")) {
+                if (System.getProperty("sun.arch.data.model").equals("32")) {
+                    // 32-bit JVM
+                    f = new File(currentPath +  "/Libs/x86/opencv_java410.dll");
+                } else {
+                    // 64-bit JVM
+                    f = new File(currentPath + "/Libs/x64/opencv_java410.dll");
+               }
+
+            } else {
+                if (OS.contains("nix") || OS.contains("nux") || OS.contains("aix")) {
+                    f = new File( "usr/lib/libopencv_java401.so");
+               }
+               /*     if (System.getProperty("sun.arch.data.model").equals("32")) {
+                        // 32-bit JVM
+                        f = new File(currentPath + "Libs\\x86\\opencv_java410.dll");
+                    } else {
+                        // 64-bit JVM
+                        f = new File(currentPath + "Libs\\x64\\opencv_java410.dll");
+                    }
+                }*/
+            }
+
+            System.load(f.getAbsolutePath());
+            Mat source = Imgcodecs.imread(path,
+                    Imgcodecs.IMREAD_COLOR);
+
+            Mat destination = new Mat(source.rows(), source.cols(), source.type());
+            Imgproc.GaussianBlur(source, destination, new Size(45, 45), 0);
+            System.out.println(currentPath + "/Images/[Processed]" +  getFilename(path));
+            Imgcodecs.imwrite(currentPath + "/Images/[Processed]" +  getFilename(path), destination);
+
+        } catch (Exception e) {
+            System.out.println("Error:" + e.getMessage());
+        }
     }
 }
